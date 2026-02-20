5a574d62360f9e432ed39b665fc48eb3b5607339
winner <umukorowinner990@gmail.com>
2026-02-20 17:26:41 +0100

    Complete Task 5 - Added REST API balance controller on port 33400

---
+++ src/main/java/com/jpmc/midascore/component/BalanceController.java
@@ -0,0 +1,25 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/balance")
+    public Balance getBalance(@RequestParam long userId) {
+        UserRecord user = userRepository.findById(userId);
+        if (user == null) {
+            return new Balance(0);
+        }
+        return new Balance(user.getBalance());
+    }
+}
